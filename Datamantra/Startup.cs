07c7d2c923fb28d6b5898d33c212310caf04f5fc
abdul.marvaan <abdul.marvaan@kiewit.com>
2017-11-18 13:13:59 -0600

    Add project files.

---
+++ Datamantra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Datamantra.Startup))]
+namespace Datamantra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
